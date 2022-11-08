cbb3a78294861601d1a300991e86384a35125796
felipe-valadares <agapitonogueira@gmail.com>
2022-11-08 00:33:31 -0300

    Todos testes concluídos

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json()
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (user.todos.length === 10 && user.pro === false) {
+    response.status(403).json()
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  if (!validate(id)) {
+    return response.status(400).json()
+  }
+
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json()
+  }
+
+  let todo = user.todos.find(todo => todo.id === id)
+  if (!todo) {
+    return response.status(404).json({error:"Todo not Found"})
+  }
+
+  request.user = user
+  request.todo = todo
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  if (!validate(id)) {
+    return response.status(400).json()
+  }
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json()
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
